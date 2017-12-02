77d9278736db398a296773bbdb4c07660614775e
Arnold Chan <34094073+coolarnie@users.noreply.github.com>
2017-12-03 00:11:39 +1100

    Create vrfy.py

---
+++ scripts/vrfy.py
@@ -0,0 +1,20 @@
+Executable File  20 lines (16 sloc)  515 Bytes
+#!/usr/bin/python
+
+import socket
+import sys
+
+if len(sys.argv) != 3:
+	print "Usage: vrfy.py <target> <users.txt>"
+	sys.exit(0)
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a Socket
+connect=s.connect((sys.argv[1],25)) # Connect to the Server
+banner=s.recv(1024)		    # Receive the banner
+print banner
+f = open(sys.argv[2], 'r')	    # open file, read only
+for user in f:			    # loop over list
+	s.send('VRFY ' + user) #VRFY a user
+	result=s.recv(1024)
+	print result
+s.close()			    # Close the socket
